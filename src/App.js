f9b5fd6fe2befb260815573fd59c5826aecb9f57
Konrad Bauer <konradbauer94@gmail.com>
2023-03-21 19:39:53 +0100

    create Header template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import { Header } from "./layoutData/Header/index";
 import { theme } from "./common/theme";
+import { Aside } from "./layoutData/Aside/index";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Header />
+      <Aside />
+      <Footer />
     </ThemeProvider>
   );
 }
--- src/layoutData/Header/styled.js
+++ src/layoutData/Header/styled.js
@@ -1,15 +1,12 @@
 import styled from "styled-components";
 
 export const Box = styled.div`
-  max-width: 90%;
+  max-width: 100%;
   margin: 0 auto;
   margin-top: 5px;
-  height: 50px;
+  height: 80px;
   display: flex;
   flex-direction: row;
-  background-color: ${({ theme }) => theme.color.deepSeaGreen};
+  background-color: ${({ theme }) => theme.color.bunker};
   border-radius: 10px;
-  -webkit-box-shadow: 22px 19px 31px -7px rgba(0, 0, 0, 1);
-  -moz-box-shadow: 22px 19px 31px -7px rgba(0, 0, 0, 1);
-  box-shadow: 22px 19px 31px -7px rgba(0, 0, 0, 1);
 `;
