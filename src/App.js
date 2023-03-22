577d58d2281ba6aa50542a4d96e55517dc41d8bd
Konrad Bauer <konradbauer94@gmail.com>
2023-03-22 17:37:42 +0100

    change structure concept to grid elements into global container and import components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import { ThemeProvider } from "styled-components";
-import { Header } from "./layoutData/Header/index";
 import { theme } from "./common/theme";
-import { Aside } from "./layoutData/Aside/index";
+import { Aside } from "./layoutData/Aside";
+import { Container } from "./layoutData/Container/styled";
 import { Footer } from "./layoutData/Footer";
+import { Header } from "./layoutData/Header";
+import { Main } from "./layoutData/Main";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      <Aside />
-      <Footer />
+      <Container>
+        <Header>
+          <h1>header</h1>
+        </Header>
+        <Aside>
+          <h2>aside</h2>
+        </Aside>
+        <Main>
+          <h1>main</h1>
+        </Main>
+        <Footer>
+          <h1>footer</h1>
+        </Footer>
+      </Container>
     </ThemeProvider>
   );
 }
