5eb4d87557fe879ba1320340940cea4afd9c4ab3
Konrad Bauer <konradbauer94@gmail.com>
2023-03-22 17:34:51 +0100

    create Main and styles

---
+++ src/layoutData/Main/index.js
@@ -0,0 +1,5 @@
+import { MainBox } from "./styled";
+
+export const Main = () => {
+  return <MainBox />;
+};
---
+++ src/layoutData/Main/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const MainBox = styled.div`
+  grid-column-start: span 2;
+  background: ${({ theme }) => theme.color.white};
+  border-radius: 3px;
+`;
