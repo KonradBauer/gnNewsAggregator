701653737d7415cdd0c255b58c5547a53f27f213
Konrad Bauer <konradbauer94@gmail.com>
2023-03-22 17:35:58 +0100

    create global container

---
+++ src/layoutData/Container/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr 2fr;
+  grid-template-rows: 70px auto 70px;
+  height: 640px;
+  width: 90%;
+  margin: 0 auto;
+  grid-gap: 3px;
+`;
