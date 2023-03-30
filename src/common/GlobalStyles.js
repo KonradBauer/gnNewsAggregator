2ac05e46165abfc324acd8f71ba426e6ccc6a3a6
Konrad Bauer <konradbauer94@gmail.com>
2023-03-30 02:16:56 +0200

    added styled scrollbar

--- src/common/GlobalStyles.js
+++ src/common/GlobalStyles.js
@@ -3,6 +3,18 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   html {
     box-sizing: border-box;
+    *::-webkit-scrollbar {
+    width: 16px;
+  }
+  *::-webkit-scrollbar-track {
+    background: #ffffff;
+  }
+  *::-webkit-scrollbar-thumb {
+    background-color: #151316;
+    border-radius: 10px;
+    border: 2px solid #efebeb;
+  }
+  
   }
 
   *, ::after, ::before {
