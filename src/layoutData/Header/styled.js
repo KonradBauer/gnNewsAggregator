ee598a887763cf652954f31a82c48b44eda6b14e
Konrad Bauer <konradbauer94@gmail.com>
2023-03-31 13:18:04 +0200

    remove hover effects on buttons in header

--- src/layoutData/Header/styled.js
+++ src/layoutData/Header/styled.js
@@ -44,11 +44,6 @@ export const ButtonView = styled.button`
   color: ${({ theme }) => theme.color.shark};
   font-weight: bold;
   cursor: pointer;
-  transition: 0.1s;
-
-  &:hover {
-    transform: scale(1.01);
-  }
 `;
 
 export const ButtonPopup = styled.button`
@@ -58,9 +53,4 @@ export const ButtonPopup = styled.button`
   color: ${({ theme }) => theme.color.shark};
   font-weight: bold;
   cursor: pointer;
-  transition: 0.1s;
-
-  &:hover {
-    transform: scale(1.01);
-  }
 `;
