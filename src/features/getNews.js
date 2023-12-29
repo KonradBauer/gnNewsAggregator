c452f44656d65aedcdcfb4bd7c55802871d8a6c1
Konrad Bauer <konradbauer94@gmail.com>
2023-12-29 18:41:32 +0100

    move api key to env

--- src/features/getNews.js
+++ src/features/getNews.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const apiKey = "apiKey=d4f58b6eeae34792956fb954c1bf4e38";
+const apiKey = process.env.REACT_APP_API_KEY;
 const address = "https://newsapi.org/v2/top-headlines?";
 
 export const getNews = (country) =>
-  axios.get(`${address}country=${country}&${apiKey}`).then((response) => response.data);
+  axios.get(`${address}country=${country}&apiKey=${apiKey}`).then((response) => response.data);
